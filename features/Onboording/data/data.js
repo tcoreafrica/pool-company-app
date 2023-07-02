0488a9f05c84a26f0517823088ec403c4d68e842
ZaydMazhour <zaydmazhour321@gmail.com>
2023-07-02 12:20:08 +0100

    onboording

---
+++ features/Onboording/data/data.js
@@ -0,0 +1,21 @@
+export const dummyData = [
+    {
+      id: 1,
+      description: 'Deliveries you can’t meet up with? Collaborate with others on the deliveries',
+      title: 'Mountain View',
+      image: 'https://randomuser.me/api/portraits/men/15.jpg'
+    },
+    {
+      id: 2,
+      description: 'Get delivery of your items in a shortest possible time',
+      title: 'Gourmet Pizza',
+      image: 'https://randomuser.me/api/portraits/men/15.jpg'
+    },
+    {
+      id: 3,
+      description: 'Get delivery of your items in a shortest possible time',
+      title: 'Colorful Artwork',
+      image: 'https://randomuser.me/api/portraits/men/15.jpg'
+    }
+  ];
+  
