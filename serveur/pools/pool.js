04b77ccbe797550d2ace5c3c9f943c1519608f90
hrichi_kabil@yahoo.com <hrichi_kabil@yahoo.com>
2023-08-30 16:00:15 +0100

    update asyncStorage-- serveur

--- serveur/pools/pool.js
+++ serveur/pools/pool.js
@@ -24,7 +24,7 @@ export const getAllExchangePool = async () => {
       };
     }
   } catch (error) {
-    console.error("Error during Fetch:", error);
+    console.error("Error during Fetch pool:", error);
     // store.dispatch(loginFailure("An error occurred"));
 
     return { success: false, error: "An error occurred" };
@@ -55,7 +55,7 @@ export const getAllMyCompanyPool = async () => {
       };
     }
   } catch (error) {
-    console.error("Error during Fetch:", error);
+    console.error("Error during Fetch pool:", error);
     // store.dispatch(loginFailure("An error occurred"));
 
     return { success: false, error: "An error occurred" };
