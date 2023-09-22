7f19681e6c5c6da7e3a84555063b4d593bb18e34
ZaydMazhour <zaydmazhour321@gmail.com>
2023-09-23 00:17:59 +0200

    colasible

---
+++ features/Orders/Componenets/CollapsableContainer.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { LayoutChangeEvent, View, Text } from "react-native";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
+
+export const CollapsableContainer = ({
+  children,
+  expanded,
+}) => {
+  const [height, setHeight] = useState(0);
+  const animatedHeight = useSharedValue(0);
+
+  const onLayout = (event) => {
+    const onLayoutHeight = event.nativeEvent.layout.height;
+
+    if (onLayoutHeight > 0 && height !== onLayoutHeight) {
+      setHeight(onLayoutHeight);
+    }
+  };
+
+  const collapsableStyle = useAnimatedStyle(() => {
+    animatedHeight.value = expanded ? withTiming(height) : withTiming(0);
+
+    return {
+      height: animatedHeight.value,
+    };
+  }, [expanded]);
+
+  return (
+    <Animated.View style={[collapsableStyle, { overflow: "hidden" }]}>
+      <View style={{ position: "absolute" }} onLayout={onLayout}>
+        {children}
+      </View>
+    </Animated.View>
+  );
+};
--- features/Orders/Componenets/Orderlist.js
+++ features/Orders/Componenets/Orderlist.js
@@ -8,10 +8,18 @@ import { AntDesign } from "@expo/vector-icons";
 import TimeLogo from "../../../constants/TimeLogo";
 import { useNavigation } from "@react-navigation/native";
 import { getAllExchangePool } from "../../../serveur/pools/pool";
- import Collapsible from "react-native-collapsible";
-const Orderlist = ({}) => {
+import Collapsible from "react-native-collapsible";
+import { useAnimatedStyle, useSharedValue, withTiming } from "react-native-reanimated";
+import { Image } from "react-native";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+const Orderlist = ({ }) => {
   const [orders, setOrders] = useState([]);
   const [expandedItem, setExpandedItem] = useState(null);
+  const [expanded, setExpanded] = useState(false);
+
+
+
 
   useEffect(() => {
     // orders.length <= 0 &&
@@ -90,76 +98,181 @@ const Orderlist = ({}) => {
   const _emptyState = () => {
     <Text>Empty state</Text>;
   };
+  const onItemPress = () => {
+    setExpanded(!expanded);
+  };
 
-  const OrderCell = ({item}) => (
-    <TouchableOpacity
-      style={{
-        height: 80,
-        flex: 1,
-        borderRadius: 2,
-        marginHorizontal: 10,
-        paddingVertical: 10,
-        paddingLeft: 40,
-        elevation: 0.5,
-      }}
-      onPress={(item) => renderDescription(item)}
-    >
-      <View style={{ position: "absolute", top: 17, left: 15 }}>
-        <PhaseLoso />
-      </View>
-
-      <View style={{}}>
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "space-between",
-            width: "85%",
-          }}
-        >
-          <Text>{item.pickUpLocation[0]}</Text>
-          <AntDesign name="arrowright" size={24} color="black" />
-          <Text style={{ fontSize: 12 }}>{item.destinationLocation[0]}</Text>
-          <Text style={{ fontWeight: "bold" }}>{item.quantity}</Text>
+  const OrderCell = ({ item }) => (
+    <View>
+      <TouchableOpacity
+        style={{
+          height: 80,
+          flex: 1,
+          borderRadius: 2,
+          marginHorizontal: 10,
+          paddingVertical: 10,
+          paddingLeft: 40,
+          elevation: 0.5,
+        }}
+        onPress={onItemPress}
+      >
+        <View style={{ position: "absolute", top: 17, left: 15 }}>
+          <PhaseLoso />
         </View>
-        <View
-          style={{
-            flexDirection: "row",
-            width: "90%",
-            justifyContent: "space-between",
-          }}
-        >
-          <View style={{ flexDirection: "row", paddingRight: 30 }}>
-            <TimeLogo style={{ marginVertical: 3 }} />
-            <Text style={{ paddingLeft: 5 }}>{item.date.substr(0, 10)}</Text>
-          </View>
 
-          <View style={{ flexDirection: "row" }}>
-            <TimeLogo style={{ marginVertical: 3 }} />
-            <Text style={{ paddingLeft: 5, color: "red" }}>Express </Text>
+        <View style={{}}>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              width: "85%",
+            }}
+          >
+            <Text>{item.pickUpLocation[0]}</Text>
+            <AntDesign name="arrowright" size={24} color="black" />
+            <Text style={{ fontSize: 12 }}>{item.destinationLocation[0]}</Text>
+            <Text style={{ fontWeight: "bold" }}>{item.quantity}</Text>
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              width: "90%",
+              justifyContent: "space-between",
+            }}
+          >
+            <View style={{ flexDirection: "row", paddingRight: 30 }}>
+              <TimeLogo style={{ marginVertical: 3 }} />
+              <Text style={{ paddingLeft: 5 }}>{item.date.substr(0, 10)}</Text>
+            </View>
+
+            <View style={{ flexDirection: "row" }}>
+              <TimeLogo style={{ marginVertical: 3 }} />
+              <Text style={{ paddingLeft: 5, color: "red" }}>Express </Text>
+            </View>
+            <TouchableOpacity
+              onPress={() => navigation.navigate("AcceptOrder", { item })}
+              style={{
+                height: 30,
+                borderRadius: 5,
+                backgroundColor: "#053582",
+                // marginHorizontal: 100,
+              }}
+            >
+              <Text
+                style={{
+                  fontWeight: "bold",
+                  color: "white",
+                  paddingVertical: 5,
+                  paddingHorizontal: 5,
+                }}
+              >
+                Accept
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+      </TouchableOpacity>
+      {expanded && (
+        <View style={{
+
+          flex: 1,
+
+          marginHorizontal: 10,
+          paddingVertical: 10,
+
+          elevation: 0.5,
+        }}>
+          <View
+            style={{ height: 85, justifyContent: 'center', backgroundColor: '#053582', borderTopLeftRadius: 11, borderTopRightRadius: 11 }}
+          >
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 10 }}>
+              <Text style={{ color: 'white', fontWeight: '400', fontSize: 14 }}>Order No: 003232</Text>
+              <Text style={{ color: 'white', fontWeight: '400', fontSize: 14 }}>2pm</Text>
+            </View>
+            <View style={{ flexDirection: 'row', paddingVertical: 6, justifyContent: 'space-between', paddingHorizontal: 10 }}>
+              <Text style={{ color: 'white', fontWeight: '700', fontSize: 16 }}>N2,500</Text>
+              <Text style={{ color: 'white', fontWeight: '700', fontSize: 14 }}>Express (1hr)</Text>
+            </View>
+          </View>
+          <View
+            style={{ flexDirection: 'row', paddingHorizontal: 20  ,marginTop : 15 , alignItems:'center' }}
+          >
+            <Text style={{ fontWeight: '700', fontSize: 14, color: '#92929D'   }}>Make this delivery for:</Text>
+            <Text style={{ fontWeight: '700', fontSize: 16 , paddingLeft : 10, textAlign :'center'  }}>N2,500</Text>
+          </View>
+          <View
+            style={{ flexDirection: 'row', paddingHorizontal: 20 , alignItems :'center'}}
+          >
+            <Text style={{ fontWeight: '700', fontSize: 14, color: '#92929D'  }}>Customer is going to pay Cash</Text>
+            <Text style={{ fontWeight: '700', fontSize: 16  , paddingLeft : 5 , textAlign :'center' }}>N3,000</Text>
+          </View>
+          <View style={{ paddingHorizontal: 20, height: 144, justifyContent: 'center', marginTop: 10 }}>
+            <View style={{ flexDirection: 'row' }}>
+              <Image source={require('../Assets/Union.png')} />
+              <Text style={{ paddingLeft: 8, color: '#92929D', fontWeight: '400' }}>Pickup</Text>
+            </View>
+            <View style={{ flexDirection: 'row', marginTop: 6 }}>
+              <Image source={require('../Assets/Frame.png')} />
+              <View>
+                <Text style={{ paddingLeft: 8, fontWeight: '700', fontSize: 14 }}>14B Wole Ariyo Street, Lekki Phase 1, Lagos</Text>
+                <Text style={{ paddingLeft: 8, color: '#92929D', fontWeight: '400' }}>Ibrahim Umar</Text>
+                <Text style={{ paddingLeft: 8, color: '#92929D', fontWeight: '400' }}>08122222222</Text>
+              </View>
+
+            </View>
+
+            <View style={{ flexDirection: 'row', marginTop: 6 }}>
+              <MaterialCommunityIcons name="radiobox-marked" size={24} color="black" />
+              <Text style={{ paddingLeft: 8, color: '#92929D', fontWeight: '400' }}>Landmark: Ancilla Hospital</Text>
+            </View>
+
+          </View>
+          <View style={{ paddingHorizontal: 20, marginTop: 10, height: 144, justifyContent: 'center', marginTop: 10 }}>
+            <View style={{ flexDirection: 'row' }}>
+              <Image source={require('../Assets/Marker.png')} />
+              <Text style={{ paddingLeft: 8, color: '#92929D', fontWeight: '400' }}>Dropoff</Text>
+            </View>
+            <View style={{ flexDirection: 'row', marginTop: 6 }}>
+              <Image source={require('../Assets/Frame.png')} />
+              <View>
+                <Text style={{ paddingLeft: 8, fontWeight: '700', fontSize: 14 }}>14B Wole Ariyo Street, Lekki Phase 1, Lagos</Text>
+                <Text style={{ paddingLeft: 8, color: '#92929D', fontWeight: '400' }}>Donald Ndonna</Text>
+                <Text style={{ paddingLeft: 8, color: '#92929D', fontWeight: '400' }}>08080808080</Text>
+              </View>
+
+            </View>
+
+            <View style={{ flexDirection: 'row', marginTop: 6 }}>
+              <MaterialCommunityIcons name="radiobox-marked" size={24} color="black" />
+              <Text style={{ paddingLeft: 8, color: '#92929D', fontWeight: '400' }}>MTN Towerhouse</Text>
+            </View>
+
           </View>
           <TouchableOpacity
-            onPress={() => navigation.navigate("AcceptOrder", { item })}
-            style={{
-              height: 30,
-              borderRadius: 5,
-              backgroundColor: "#053582",
-              // marginHorizontal: 100,
-            }}
+
+            style={{ height: 50, justifyContent:'center',backgroundColor: "#053582", borderRadius: 10, marginHorizontal : 20 , marginTop : 15 , marginBottom : 15}}
           >
             <Text
               style={{
-                fontWeight: "bold",
                 color: "white",
-                paddingVertical: 5,
-                paddingHorizontal: 5,
+                fontWeight: "700",
+                textAlign : 'center',
+                
+                fontSize: 15,
               }}
             >
-              Accept
+              Accept Order
             </Text>
           </TouchableOpacity>
+
+
         </View>
-      </View>
-    </TouchableOpacity>
+
+
+      )}
+    </View>
+
   );
 
   const renderDescription = (item) => (
